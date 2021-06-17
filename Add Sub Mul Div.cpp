86d6c4418669b9f6fa069f86b48deef026e9c8e5
AyushShahh <71313814+AyushShahh@users.noreply.github.com>
2021-06-17 15:26:28 +0530

    Updated code
    Returns NOT DEFINED on division if denominator is zero.

--- Add Sub Mul Div.cpp
+++ Add Sub Mul Div.cpp
@@ -9,15 +9,31 @@ int main()
 	printf("Enter two numbers: ");
 	scanf("%d %d",&a,&b);
 	
-	add=a+b;
-	sub=a-b;
-	mul=a*b;
-	div=a/(float)b;
+	if(b != 0)
+	{
+		add=a+b;
+		sub=a-b;
+		mul=a*b;
+		div=a/(float)b;
+		
+		printf("\n\n Addition = %d\n",add);
+		printf("\n Subtraction = %d\n",sub);
+		printf("\n Multiplication = %d\n",mul);
+		printf("\n Division = %.2f\n",div);	
+	}
+
+	else
+	{
+		add=a+b;
+		sub=a-b;
+		mul=a*b;
+		
+		printf("\n Addition = %d\n",add);
+		printf("\n Subtraction = %d\n",sub);
+		printf("\n Multiplication = %d\n",mul);
+		printf("\n Division = NOT DEFINED\n");
 	
-	printf("\n\n Addition = %d\n",add);
-	printf("\n Subtraction = %d\n",sub);
-	printf("\n Multiplication = %d\n",mul);
-	printf("\n Division = %.2f\n",div);
+	}
 	
 	getch();
 	return 0;
